ece34cf386ecf2c75167cb495810141d78135ec2
moonseokchoi-kr <moonseoktech@gmail.com>
2021-04-18 22:57:15 +0900

    create vkInstance and layer

--- VulkanRenderer/helloTriangleApplication.h
+++ VulkanRenderer/helloTriangleApplication.h
@@ -7,25 +7,58 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
+
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
+const std::vector<const char*> validationLayers = {
+	"VK_LAYER_KHRONOS_validation"
+};
+
+#ifdef NDEBUG
+const bool enableValidationLayers = false;
+#else
+const bool enableValidationLayers = true;
+#endif
+
+
+
+
+
 class HelloTriangleApplication
 {
 public:
 	void run()
 	{
 		initVulkan();
+		createInstance();
 		mainLoop();
 		cleanup();
 	}
 private:
 	void initWindow();
 	void initVulkan();
+	void setupDebugMessenger();
+	void createInstance();
+	bool checkValidationLayerSupport();
+	std::vector<const char*> getRequiredExtensions();
+	VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger);
+	void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator);
 	void mainLoop();
 	void cleanup();
-
+	
 
 private:
 	const uint32_t WIDTH = 800;
 	const uint32_t HEIGHT = 600;
 	GLFWwindow* window;
+	VkInstance instance;
+	VkDebugUtilsMessengerEXT debugMessenger;
 };
 
+static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData) {
+	std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+
+	return VK_FALSE;
+}
