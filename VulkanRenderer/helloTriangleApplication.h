36ec3aa9e434018b5fd74ee8b7af7b7e78b1a0c1
moonseokchoi-kr <moonseoktech@gmail.com>
2021-04-23 22:22:49 +0900

    complete draw triangle

--- VulkanRenderer/helloTriangleApplication.h
+++ VulkanRenderer/helloTriangleApplication.h
@@ -14,8 +14,10 @@
 #include <set>
 #include <cstdint> // Necessary for UINT32_MAX
 #include <algorithm> // Necessary for std::min/std::max
+#include <fstream>
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
+const int MAX_FRAMES_IN_FLIGHT = 2;
 
 const std::vector<const char*> validationLayers = {
 	"VK_LAYER_KHRONOS_validation"
@@ -83,12 +85,21 @@ private:
 	VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities);
 
 	void createImageView();
+
+	void createRenderPass();
 	void createGraphicsPipeline();
+	void createFrameBuffer();
+	void createCommandPool();
+	void createCommendBuffer();
+	void createSyncObjects();
+	VkShaderModule createShaderModule(const std::vector<char>& code);
+
 	std::vector<const char*> getRequiredExtensions();
 	bool checkValidationLayerSupport();
 	bool checkDeviceExtensionSupport(VkPhysicalDevice device);
 	
 	void mainLoop();
+	void drawFrame();
 	void cleanup();
 	
 
@@ -115,12 +126,41 @@ private:
 	VkQueue graphicsQueue;
 	VkQueue presentQueue;
 
+	VkRenderPass renderPass;
+	VkPipelineLayout pipelineLayout;
+	VkPipeline graphicsPipeline;
+	VkCommandPool commandPool;
+	std::vector<VkCommandBuffer> commandBuffers;
+	std::vector<VkFramebuffer> swapChainFramebuffers;
 
-	
+	std::vector<VkSemaphore> imageAvailableSemaphores;
+	std::vector<VkSemaphore> renderFinishedSemaphores;
+	std::vector<VkFence> inFlightFences;
+	std::vector<VkFence> imagesInFlight;
+	size_t currentFrame = 0;
 };
 
 static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData) {
 	std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
 
 	return VK_FALSE;
+}
+
+static std::vector<char> readFile(const std::string& filename) {
+	std::ifstream file(filename, std::ios::ate | std::ios::binary);
+
+	if (!file.is_open())
+	{
+		throw std::runtime_error("failed to open file");
+	}
+
+	size_t filesize = (size_t)file.tellg();
+	std::vector<char> buffer(filesize);
+
+	file.seekg(0);
+	file.read(buffer.data(), filesize);
+
+	file.close();
+
+	return buffer;
 }
